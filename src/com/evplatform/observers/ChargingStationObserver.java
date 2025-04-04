43baab8aee18592b4982cbbad628bc5ab67cd1f3
adonniss <132979894+adonnisss@users.noreply.github.com>
2025-04-04 12:17:28 +0200

    Delete src/com/evplatform directory

--- src/com/evplatform/observers/ChargingStationObserver.java
+++
@@ -1,7 +0,0 @@
-package com.evplatform.observers;
-
-import com.evplatform.vao.ChargingStation;
-
-public interface ChargingStationObserver {
-    void update(ChargingStation station, ChargingStation.ChargingStationStatus oldStatus);
-}
